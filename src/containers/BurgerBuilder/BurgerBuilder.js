c622ab42366df007316fea12476c9d6502ec04ad
[Dorijan] <[dorijan11@gmail.com]>
2019-06-20 18:29:43 +0200

    changed the logic of updating if an order is available

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions'
 
 class BurgerBuilder extends Component {
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -40,7 +39,7 @@ class BurgerBuilder extends Component {
         return sum + el;
       }, 0);
 
-    this.setState({ purchasable: sum > 0 }) // Switches purchasable depending on the valuees of the ingredients
+    return  sum > 0  // Switches purchasable depending on the valuees of the ingredients
   }
  
 
@@ -90,7 +89,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             price={this.props.price}
             ordered={this.purchaseHandler} />
 
