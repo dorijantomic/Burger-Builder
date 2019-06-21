ed2034a5d8ab5ee51971c6213ab9189b1a1cac91
[Dorijan] <[dorijan11@gmail.com]>
2019-06-21 17:51:38 +0200

    finished implementing basic redux structure for our app

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import classes from './ContactData.module.css';
 
@@ -105,7 +106,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
            
@@ -202,4 +203,12 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+  
+}
+
+export default connect(mapStateToProps)(ContactData);
